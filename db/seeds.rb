f4e84f7fd5b2a3b1ae6d8a159717a0540180669e
rishavkrd <rishavkrd11@tamu.edu>
2022-09-16 15:59:03 -0500

    seed_dump added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([
+  {title: "Rishav's book", author: "Rishav", price: "1.25", published_date: "2022-09-21"},
+  {title: "Superman Vol 1", author: "DC Comics", price: "34.2", published_date: "2022-06-16"},
+  {title: "Test Book #3", author: "Ghost", price: "2.03", published_date: "2014-06-11"},
+  {title: "Think and Grow Rich", author: "Napoleon Hill", price: "1.25", published_date: "1982-01-05"}
+])
+User.create!([
+  {username: "Rishavkrd"},
+  {username: "Batman"},
+  {username: "Tom"}
+])
+UserBook.create!([
+  {user_id: 1, book_id: 1},
+  {user_id: 2, book_id: 2},
+  {user_id: 3, book_id: 4},
+  {user_id: 1, book_id: 4}
+])
